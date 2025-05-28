b78267a23159be405045aca21b46a021d1bb7d1e
Abdullah Ahmed <aahmed@outlook.com>
2025-05-28 22:41:41 +0500

    Refactored from nodemailer to API for transactional emails due to VPS restrictions

--- server/src/utils/emailService.js
+++ server/src/utils/emailService.js
@@ -7,54 +7,59 @@ const oAuth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_REDIRECT_URI,
 );
 
-console.log('Refresh Token:', process.env.GOOGLE_REFRESH_TOKEN);
-
 oAuth2Client.setCredentials({
     refresh_token: process.env.GOOGLE_REFRESH_TOKEN
 });
 
+function makeRawMessage(from, to, subject, html) {
+    const messageParts = [
+        `From: ${from}`,
+        `To: ${to}`,
+        `Subject: ${subject}`,
+        'Content-Type: text/html; charset=utf-8',
+        '',
+        html,
+    ];
+    const message = messageParts.join('\n');
 
-async function createTransporter() {
-    const accessToken = await oAuth2Client.getAccessToken();
+    return Buffer.from(message)
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
 
-    return nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
-        auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL_USER, 
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-            accessToken: accessToken.token || accessToken, 
-          }
+async function sendEmailAPI(to, subject, html) {
+    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+  
+    const raw = makeRawMessage(process.env.EMAIL_FROM, to, subject, html);
+  
+    const res = await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: {
+        raw,
+      },
     });
+  
+    return res.data;
 }
 
 const sendVerificationCode = async (email, name, code) => {
-
-    const mailOptions = {
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject: 'Confra email verification',
-        html: `
+    const subject = 'Confra email verification';
+    const html = `
         <h1>Your verification code</h1>
         <p>Hi ${name}:</p>
         <p>You're almost there! Please enter the code below to verify your email address:</p>
         <h1 style="font-size: 24px; letter-spacing: 2px; color: #333;">${code}</h1>
         <p>This code will expire in 10 minutes.</p>
         <p>If you did not request this verification, please ignore this email.</p>
-        `
-    };
-    const transporter = await createTransporter();
-    await transporter.sendMail(mailOptions);
+        `;
+    await sendEmailAPI(email, subject, html);
 }
 
 const sendConfirmation = async ({email, event, qrCodeDataUrl}) => {
-    const mailOptions = {
-        from: process.env.EMAIL_FROM,
-        to: email, 
-        subject: `Confra Event Confirmation: ${event.title}`,
-        html: `
+    const subject = `Confra Event Confirmation: ${event.title}`
+    const html = `
             <h1>Registration Confirmed</h1>
             <p>You're all set to attend ${event.title}!</p>
             <h2>Event Details:</h2>
@@ -68,21 +73,12 @@ const sendConfirmation = async ({email, event, qrCodeDataUrl}) => {
             <img src="${qrCodeDataUrl}" alt="Event QR Code" />
             <p>Please show this QR code at the event.</p>
             <p>We look forward to seeing you there!</p>
-        `
-    };
-    const transporter = await createTransporter();
-    await transporter.sendMail(mailOptions);
+        `;
+    await sendEmailAPI(email, subject, html);
 }
 
-const sendEmail = async ({ to, subject, html }) => {
-    const mailOptions = {
-        from: process.env.EMAIL_FROM,
-        to,
-        subject,
-        html
-    };
-    const transporter = await createTransporter();
-    await transporter.sendMail(mailOptions);
+const sendEmail = async ({ email, subject, html }) => {
+    await sendEmailAPI(email, subject, html);
 }
 
 module.exports = { sendVerificationCode, sendConfirmation };
